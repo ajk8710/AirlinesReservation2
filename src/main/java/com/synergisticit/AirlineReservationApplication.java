a03ab8ee88e95d8aac7aae2a93f2dae4e256f83b
Alex Kim <ajk8710@nyu.edu>
2024-04-06 18:34:41 -0400

    Application to extend SpringBootServletInitializer.

--- src/main/java/com/synergisticit/AirlineReservationApplication.java
+++ src/main/java/com/synergisticit/AirlineReservationApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AirlineReservationApplication {
+public class AirlineReservationApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirlineReservationApplication.class, args);
+    public static void main(String[] args) {
+	    SpringApplication.run(AirlineReservationApplication.class, args);
 	}
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AirlineReservationApplication.class);
+    }
 }
